ff9a0120244ab183e8cd46f922b8794b55ec22cc
emeryberger <emery.berger@gmail.com>
2019-03-14 11:12:34 -0700

    We can get all properties at once. Done.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -27,17 +27,11 @@ export default class App extends React.Component<AppProps, AppState> {
 		let usedRange = currentWorksheet.getUsedRange();
 		let everythingRange = currentWorksheet.getRange();
 		await context.sync();
-		// Clear all formatting. Really we want to just clear colors but fine for now (FIXME later).
-		everythingRange.clear(Excel.ClearApplyTo.formats);
 		// Now get the addresses, the formulas, and the values.
-                usedRange.load('address');
+                usedRange.load(['address', 'formulas', 'values']);
 		await context.sync();
 		let address = usedRange.address;
-                usedRange.load('formulas'); // note that real formulas start with "="
-                await context.sync();
 		let formulas = usedRange.formulas;
-                usedRange.load('values');
-		await context.sync();
 		let values = usedRange.values;
 		// Now we can get the formula ranges (all cells with formulas),
 		// and the numeric ranges (all cells with numbers). These come in as 2-D arrays.
@@ -45,11 +39,14 @@ export default class App extends React.Component<AppProps, AppState> {
 		let numericRanges = usedRange.getSpecialCells(Excel.SpecialCellType.constants,
 		    Excel.SpecialCellValueType.numbers);
 		await context.sync();
+		// First, clear all formatting. Really we want to just clear colors but fine for now (FIXME later)
+		everythingRange.clear(Excel.ClearApplyTo.formats);
 		// For fun, make all formulas pink and all numbers yellow.
 		formulaRanges.format.fill.color = "pink";
 		numericRanges.format.fill.color = "yellow";
 		await context.sync();
                 console.log(`The range address was ${address}.`);
+//		console.log(`The fudge potato was ${columnIndex}.`);
 		console.log(JSON.stringify(formulas, null, 4));
 		console.log(JSON.stringify(values, null, 4));
             });
