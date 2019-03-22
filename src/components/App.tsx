7c5b80a493f733abe95b5d6c3aa79864503bf849
emeryberger <emery.berger@gmail.com>
2019-03-21 17:06:48 -0700

    Ritual.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -27,7 +27,7 @@ export default class App extends React.Component<AppProps, AppState> {
 
 	// Now group them (by COLUMNS).
 	let grouped_ranges = Colorize.group_ranges(identified_ranges);
-	console.log(grouped_ranges);
+	// console.log(grouped_ranges);
 	// FINALLY, process the ranges.
 	Object.keys(grouped_ranges).forEach(color => {
 	    if (!(color === undefined)) {
@@ -42,6 +42,7 @@ export default class App extends React.Component<AppProps, AppState> {
 		    let range = currentWorksheet.getRange(col0 + row0 + ":" + col1 + row1);
 		    // console.log("setting " + col0 + row0 + ":" + col1 + row1 + " to " + color);
 		    range.format.fill.color = color;
+//		    range.untrack();
 		}
 	    }
 	})
@@ -50,6 +51,8 @@ export default class App extends React.Component<AppProps, AppState> {
     setColor = async () => {
         try {
             await Excel.run(async context => {
+		let app = context.workbook.application;
+		console.log("ExceLint: starting processing.");
 		let startTime = performance.now();
 		
 	    	let currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
@@ -57,18 +60,46 @@ export default class App extends React.Component<AppProps, AppState> {
 		let everythingRange = currentWorksheet.getRange();
 		// Now get the addresses, the formulas, and the values.
                 usedRange.load(['address', 'formulas', 'values']);
+		currentWorksheet.charts.load(['items']);
 		
 		await context.sync();
+		console.log("ExceLint: done with sync 1.");
 
 		let address = usedRange.address;
 	
 		// Now we can get the formula ranges (all cells with formulas),
 		// and the numeric ranges (all cells with numbers). These come in as 2-D arrays.
-		let formulaRanges = usedRange.getSpecialCellsOrNullObject(Excel.SpecialCellType.formulas);
+//		let formulaRanges = usedRange.getSpecialCellsOrNullObject(Excel.SpecialCellType.formulas);
 		let numericRanges = usedRange.getSpecialCellsOrNullObject(Excel.SpecialCellType.constants,
 							      Excel.SpecialCellValueType.numbers);
 		let formulas = usedRange.formulas;
 		let values = usedRange.values;
+		numericRanges.format.borders.load(['items']);
+
+		/*
+		let chart = currentWorksheet.charts.getItemAt(0);
+		chart.load(['series']);
+		*/
+
+		await context.sync();
+		console.log("ExceLint: done with sync 2.");
+
+
+// 		console.log(chart.series.getItemAt(0));
+		
+		/*
+		// Extract dependencies from charts.
+		for (let i = 0; i < currentWorksheet.charts.count; i++) {
+		    let chart = currentWorksheet.charts.getItemAt(i);
+		    chart.load(['series']);
+		    await context.sync();
+		    console.log(JSON.stringify(chart));
+//		    console.log(JSON.stringify(chart.series));
+//		    chart.load(['series']);
+//		    await context.sync();
+//		    console.log(JSON.stringify(chart.series));
+		}
+		*/
 		
 		// FIX ME - need a button to restore all formatting.
 		// First, clear all formatting. Really we want to just clear colors but fine for now (FIXME later)
@@ -77,20 +108,24 @@ export default class App extends React.Component<AppProps, AppState> {
 		// Make all numbers yellow; this will be the default value for unreferenced data.
 		numericRanges.format.fill.color = "yellow";
 
+//		await context.sync();
+
 		// Give every numeric data item a dashed border.
-		numericRanges.format.borders.load(['items']);
-		await context.sync();
 		let items = numericRanges.format.borders.items;
-		for (let border of items) {
-		    border.weight = "Thick";
-		    border.style = "Dash";
-		    border.tintAndShade = -1;
-		}
 		
+		for (let border of items) {
+		    border.set ({ "weight" : "Thin",
+				  "style" : "Dash",
+				  "tintAndShade" : -1 });
+		}
+
+		
+		// numericRanges.untrack();
 
 		let [sheetName, startCell] = Colorize.extract_sheet_cell(address);
 		let vec = Colorize.cell_dependency(startCell, 0, 0);
-		let processed_formulas = Colorize.process_formulas(formulas, vec[0]-1, vec[1]-1);
+		
+ 		let processed_formulas = Colorize.process_formulas(formulas, vec[0]-1, vec[1]-1);
 		let processed_data = Colorize.color_all_data(formulas, processed_formulas, vec[0], vec[1]);
 		
 		this.process(processed_data, currentWorksheet);
@@ -98,6 +133,7 @@ export default class App extends React.Component<AppProps, AppState> {
 	
 		
 		await context.sync();
+		console.log("ExceLint: done with sync 3.");
 		
 		let endTime = performance.now();
 		let timeElapsedMS = endTime - startTime;
