20fbdb29ed63235348538ecd75d5d3e65d99248f
emeryberger <emery.berger@gmail.com>
2019-03-23 12:58:17 -0700

    For now, disabled attempts to save formatting.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -27,19 +27,52 @@ export default class App extends React.Component<AppProps, AppState> {
 
     
     private async processRange(context : any, currentWorksheet : any, startCol : number, startRow : number, endCol : number, endRow : number) {
-	let startCell = Colorize.column_index_to_name(startCol) + startRow;
-	let endCell = Colorize.column_index_to_name(endCol) + endRow;
-	let range = currentWorksheet.getRange(startCell + ":" + endCell);
-	// 	    await context.sync();
-	range.format.fill.load(['color']);
-	await context.sync();
-	let color = range.format.fill.color;
-	if (color !== null) {
-	    this.savedColors.push([startCell, endCell, color]);
-	} else {
-	    if ((endCol - startCol >= 0) && (endRow - startRow >= 0)) {
-		let midCol = startCol + Math.floor((endCol - startCol) / 2);
-		let midRow  = startRow + Math.floor((endRow - startRow) / 2);
+ 	if ((endCol - startCol >= 0) && (endRow - startRow >= 0)) {
+	    let startCell = Colorize.column_index_to_name(startCol) + startRow;
+	    let endCell = Colorize.column_index_to_name(endCol) + endRow;
+	    let range = [] as any;
+	    if (startCell === endCell) {
+		range = currentWorksheet.getCell(startRow, startCol);
+	    } else {
+		/* // iterative version
+		for (let c = startCol; c <= endCol; c++) {
+		    for (let r = startRow; r <= endRow; r++) {
+			range = currentWorksheet.getCell(c, r);
+			range.format.fill.load(['color']);
+			console.log("synching for "+c+ " " +r);
+			await context.sync();
+			let color = range.format.fill.color;
+			let startCell = Colorize.column_index_to_name(c) + r;
+			this.savedColors.push([startCell, startCell, color]);
+		    }
+		}
+		*/
+		range = currentWorksheet.getRange(startCell + ":" + endCell);
+	    }
+//	    return;
+	    // 	    await context.sync();
+	    range.format.fill.load(['color']);
+	    console.log("synching for "+startCell+" through " + endCell);
+	    await context.sync();
+	    let color = range.format.fill.color;
+	    if (color !== null) {
+		this.savedColors.push([startCell, endCell, color]);
+	    } else {
+		/* // column by column
+		if (startCol !== endCol) {
+		    let midCol = startCol + Math.floor((endCol - startCol) / 2);		    
+		    await this.processRange(context, currentWorksheet, startCol, startRow, midCol, endRow);
+		    await this.processRange(context, currentWorksheet, midCol + 1, startRow, endCol, endRow);
+		} else {
+		    let midRow  = startRow + Math.floor((endRow - startRow) / 2);
+		    await this.processRange(context, currentWorksheet, startCol, startRow, endCol, midRow);
+		    await this.processRange(context, currentWorksheet, startCol, midRow + 1, endCol, endRow);
+		}
+		*/
+		// binary search
+		let divisor = 2; // (Math.random() * (8 - 2)) + 2.0001;
+ 		let midCol = startCol + Math.floor((endCol - startCol) / divisor);
+		let midRow  = startRow + Math.floor((endRow - startRow) / divisor);
 		await this.processRange(context, currentWorksheet, startCol, startRow, midCol, midRow);
 		await this.processRange(context, currentWorksheet, midCol + 1, startRow, endCol, midRow);
 		await this.processRange(context, currentWorksheet, startCol, midRow + 1, midCol, endRow);
@@ -48,9 +81,10 @@ export default class App extends React.Component<AppProps, AppState> {
 	}
     }
     
-    clearColor = async () => {
+    attemptToSaveColor = async () => {
 	try {
 	    await Excel.run(async context => {
+		let startTime = performance.now();
 		this.savedColors = [];
 		// Load up the used range.
 	    	let currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
@@ -72,6 +106,9 @@ export default class App extends React.Component<AppProps, AppState> {
 		    await this.processRange(context, currentWorksheet, startCol, startRow, endCol, endRow);
 		}
 		console.log(this.savedColors);
+		let endTime = performance.now();
+		let timeElapsedMS = endTime - startTime;
+ 		console.log("Time elapsed (ms) = " + timeElapsedMS);
 	    });
 	} catch (error) {
             OfficeHelpers.UI.notify(error);
@@ -105,7 +142,7 @@ export default class App extends React.Component<AppProps, AppState> {
 	})
     }
     
-    OldclearColor = async () => {
+    clearColor = async () => {
         try {
             await Excel.run(async context => {
 
@@ -203,7 +240,7 @@ export default class App extends React.Component<AppProps, AppState> {
 		
 		let endTime = performance.now();
 		let timeElapsedMS = endTime - startTime;
-		console.log("Time elapsed (ms) = " + timeElapsedMS);
+ 		console.log("Time elapsed (ms) = " + timeElapsedMS);
             });
         } catch (error) {
             OfficeHelpers.UI.notify(error);
