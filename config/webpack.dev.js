febb10f3bdb67856ce07787f82829f727a302ad5
emeryberger <emery.berger@gmail.com>
2019-05-04 13:32:41 -0700

    Added use of custom certs, azure allowed port.

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,10 +1,12 @@
+const devCerts = require("office-addin-dev-certs");
 const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 
-module.exports = webpackMerge(commonConfig, {
+module.exports = async (env, options) => {
+    const config = webpackMerge(commonConfig, {
     devtool: 'eval-source-map',
     devServer: {
         publicPath: '/',
@@ -15,17 +17,19 @@ module.exports = webpackMerge(commonConfig, {
             warnings: false,
             errors: true
         },
-          https: {
-            key: fs.readFileSync('./certs/server.key'),
-            cert: fs.readFileSync('./certs/server.crt'),
-            cacert: fs.readFileSync('./certs/ca.crt')
-        },
         historyApiFallback: true,
+	headers: {
+            "Access-Control-Allow-Origin": "*"
+	},
+	https: await devCerts.getHttpsServerOptions(),
+	port: 3000,
 	allowedHosts: [
-            '.amazonaws.com'
+            '.amazonaws.com', 'azure.com'
         ]
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin()
-    ]
-});
+    ]});
+    return config;
+}
+
