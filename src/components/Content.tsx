ceef23f8c84d0b610a3724a4c5e8c2b9f2ddf84d
emeryberger <emery.berger@gmail.com>
2019-05-23 10:37:14 -0700

    Removed commented-out section.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -47,12 +47,6 @@ export class Content extends React.Component<ContentProps, any> {
 					<rect x="0" y="0" width="50" height="20" fill="darkgreen" />
 					<text x="55" y="13">formulas (dark colors)</text>
 			</svg>
-			/*
-				<svg width="300" height="20">
-					<rect x="0" y="0" width="50" height="20" fill="lightgreen" />
-					<text x="55" y="13">data (light colors)</text>
-				</svg>
-			*/
 				<svg width="300" height="20">
 					<rect x="0" y="0" width="50" height="20" fill="#eed202" />
 					<text x="55" y="13">unreferenced data</text>
