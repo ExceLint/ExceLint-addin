13d3baf04ffe2134fe35ac69bcec81c5735d80f4
emeryberger <emery.berger@gmail.com>
2019-05-24 14:33:36 -0700

    Try try again.

--- src/components/colorize.js
+++ src/components/colorize.js
@@ -87,7 +87,7 @@ var Colorize = /** @class */ (function () {
             //		    console.log("process_formulas: formulas[" + i + "] = " + JSON.stringify(row));
             for (var j = 0; j < row.length; j++) {
                 if ((row[j].length > 0) && (row[j][0] === '=')) {
-                    //				let cell = row[j];
+                    var cell = row[j];
                     //				cell = cell.replace('IMLOG2',''); // kind of a hack for now
                     //				    console.log("process_formulas: i = " + i + ", j = " + j);
                     //				    console.log("process_formulas: origin_col, row = " + origin_col + ", " + origin_row);
--- src/components/colorize.ts
+++ src/components/colorize.ts
@@ -99,7 +99,7 @@ export class Colorize {
 //		    console.log("process_formulas: formulas[" + i + "] = " + JSON.stringify(row));
 			for (let j = 0; j < row.length; j++) {
 			    if ((row[j].length > 0) && (row[j][0] === '=')) {
-//				let cell = row[j];
+				let cell = row[j];
 //				cell = cell.replace('IMLOG2',''); // kind of a hack for now
 				
 //				    console.log("process_formulas: i = " + i + ", j = " + j);
