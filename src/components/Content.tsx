62fec69937ec7e1879962694493d6a7018e1e880
emeryberger <emery.berger@gmail.com>
2019-06-01 15:50:53 -0700

    One more newline

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -83,6 +83,7 @@ export class Content extends React.Component<ContentProps, any> {
 					<Button className='normal-button' buttonType={ButtonType.hero} onClick={this.props.click1}>{this.props.buttonLabel1}</Button>
 			<Button className='ms-button' buttonType={ButtonType.hero} onClick={this.props.click2}>{this.props.buttonLabel2}</Button>
 			<br />
+			<br />
 			<DisplayFixes currentFix={this.state.currentFix} totalFixes={this.state.totalFixes} themFixes={this.state.themFixes} selector={this.props.selector} />
 				Click on <a onClick={this.props.click1}><b>Reveal Structure</b></a> to reveal the underlying structure of the spreadsheet.
 				Different formulas are assigned different colors, making it easy to spot inconsistencies or to audit a spreadsheet for correctness.
