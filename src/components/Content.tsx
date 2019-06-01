142be09b38b0114a8043d667f0324d6da2f80550
emeryberger <emery.berger@gmail.com>
2019-06-01 15:13:16 -0700

    Fixing padding.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -79,7 +79,7 @@ export class Content extends React.Component<ContentProps, any> {
     render() {
 		return (
 			<div id='content-main'>
-			<div id='padding'>
+			<div className='padding'>
 					<Button className='normal-button' buttonType={ButtonType.hero} onClick={this.props.click1}>{this.props.buttonLabel1}</Button>
 					<Button className='ms-button' buttonType={ButtonType.hero} onClick={this.props.click2}>{this.props.buttonLabel2}</Button>
 				</div>
