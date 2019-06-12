7c5f1a35bae6a32290a7f3427751b444031e4bfa
emeryberger <emery.berger@gmail.com>
2019-06-12 14:46:02 -0700

    Backed out log factor.

--- src/components/colorize.js
+++ src/components/colorize.js
@@ -243,7 +243,7 @@ var Colorize = /** @class */ (function () {
         var norm_max = Math.max(merge_with_norm, target_norm);
         var fix_distance = Math.abs(norm_max - norm_min) / this.Multiplier;
         var entropy_drop = this.entropydiff(n_min, n_max);
-        var ranking = -(1.0 - entropy_drop) / Math.log2(fix_distance * n_min + 1.0);
+        var ranking = -(1.0 - entropy_drop) / (fix_distance * n_min + 1.0);
         return ranking;
     };
     Colorize.count_proposed_fixes = function (fixes) {
