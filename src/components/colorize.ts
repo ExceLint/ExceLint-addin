4ace9821978eef8b8b24dfd2f18f30fc31771735
emeryberger <emery.berger@gmail.com>
2019-06-12 14:49:00 -0700

    Sanity checking.

--- src/components/colorize.ts
+++ src/components/colorize.ts
@@ -263,7 +263,7 @@ export class Colorize {
  	let norm_max = Math.max(merge_with_norm, target_norm);
 	let fix_distance = Math.abs(norm_max - norm_min) / this.Multiplier;
 	let entropy_drop = this.entropydiff(n_min, n_max);
-	let ranking = -(1.0 - entropy_drop) / (fix_distance * n_min + 1.0);
+	let ranking = -(1.0 - entropy_drop); //  / (fix_distance * n_min + 1.0);
 	return ranking;
     }
 
