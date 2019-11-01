950345d54361629a95296de1f9256110c48251d1
emeryberger <emery.berger@gmail.com>
2019-11-01 16:31:53 -0400

    Check if in node.

--- src/components/timer.ts
+++ src/components/timer.ts
@@ -1,29 +1,41 @@
 export class Timer {
 
     private name: string;
+    private perf: any;
     private startTimeMS: number;
     private splitTimeMS: number;
     
     constructor(name: string) {
 	this.name = name;
-	this.startTimeMS = performance.now();
+	this.perf = null;
+	if (typeof window === 'undefined') {
+	    this.perf = {
+		now: function() {
+		    let [secs, nanosecs] = process.hrtime()
+		    return ((secs * 1e9) + nanosecs) / 1e6;
+		}
+	    }
+	} else {
+	    this.perf = performance;
+	}
+	this.startTimeMS = this.perf.now();
 	this.splitTimeMS = this.startTimeMS;
     }
 
     public start(): void {
-	this.startTimeMS = performance.now();
+	this.startTimeMS = this.perf.now();
 	this.splitTimeMS = this.startTimeMS;
     }
 
     public split(location: string): void {
-	let curr = performance.now();
+	let curr = this.perf.now();
 	let elapsed = curr - this.splitTimeMS;
 	console.log("timer: " + this.name + " @ " + location + " : = " + this.roundMe(elapsed) + " (total = " + this.roundMe(curr - this.startTimeMS) + ")");
 	this.splitTimeMS = curr;
     }
 
     public elapsedTime() : number {
-	return performance.now() - this.startTimeMS;
+	return this.perf.now() - this.startTimeMS;
     }
 
     private roundMe(v: number) : number {
