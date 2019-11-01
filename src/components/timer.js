e080a2a0f16de75b2f2cf43488e959addd7c058c
emeryberger <emery.berger@gmail.com>
2019-11-01 16:31:54 -0400

    Check if in node.

--- src/components/timer.js
+++ src/components/timer.js
@@ -3,21 +3,33 @@ exports.__esModule = true;
 var Timer = /** @class */ (function () {
     function Timer(name) {
         this.name = name;
-        this.startTimeMS = performance.now();
+        this.perf = null;
+        if (typeof window === 'undefined') {
+            this.perf = {
+                now: function () {
+                    var _a = process.hrtime(), secs = _a[0], nanosecs = _a[1];
+                    return ((secs * 1e9) + nanosecs) / 1e6;
+                }
+            };
+        }
+        else {
+            this.perf = performance;
+        }
+        this.startTimeMS = this.perf.now();
         this.splitTimeMS = this.startTimeMS;
     }
     Timer.prototype.start = function () {
-        this.startTimeMS = performance.now();
+        this.startTimeMS = this.perf.now();
         this.splitTimeMS = this.startTimeMS;
     };
     Timer.prototype.split = function (location) {
-        var curr = performance.now();
+        var curr = this.perf.now();
         var elapsed = curr - this.splitTimeMS;
         console.log("timer: " + this.name + " @ " + location + " : = " + this.roundMe(elapsed) + " (total = " + this.roundMe(curr - this.startTimeMS) + ")");
         this.splitTimeMS = curr;
     };
     Timer.prototype.elapsedTime = function () {
-        return performance.now() - this.startTimeMS;
+        return this.perf.now() - this.startTimeMS;
     };
     Timer.prototype.roundMe = function (v) {
         return Math.round(v * 100) / 100;
