e63611e68354c63a9c5dac601f5112ab819b7b6d
emeryberger <emery.berger@gmail.com>
2019-12-05 17:41:48 -0500

    Porting to TS continues.

--- experimental/driver.ts
+++ experimental/driver.ts
@@ -1,7 +1,9 @@
-import { InfoGain } from './infogain';
+import { InfoGain, Stencil } from './infogain';
 
 console.log(InfoGain.normalized_entropy([25]));
 console.log(InfoGain.normalized_entropy([25, 1]));
 console.log(InfoGain.normalized_entropy([25, 1, 1, 1]));
 console.log(InfoGain.salience([25, 1], 0));
 console.log(InfoGain.salience([25, 1], 1));
+let arr: Array<Array<number>> = [[1, 1, 1], [1, 2, 1], [1, 1, 1]];
+console.log(Stencil.stencil_computation(arr, (x: number, y: number) => { return x + y; }, 0));
