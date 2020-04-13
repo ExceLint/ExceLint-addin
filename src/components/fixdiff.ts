9053d3b364e1e4a0392206a95afdfe85ff507785
emeryberger <emery.berger@gmail.com>
2020-04-13 18:13:22 -0400

    Some cleanup.

--- src/components/fixdiff.ts
+++ src/components/fixdiff.ts
@@ -8,16 +8,14 @@ import { ExcelUtils } from './excelutils';
 
 class CellEncoder {
 
-    private static maxRows : number = 64;
-    private static maxColumns : number = 32;
+    private static maxRows : number = 64; // -32..32
+    private static maxColumns : number = 32; // -16..16
     private static absoluteRowMultiplier : number = 2 * CellEncoder.maxRows * CellEncoder.maxColumns;  // if bit set, absolute row
-    private static absoluteColumnMultiplier : number = 2 * CellEncoder.absoluteRowMultiplier;          // if bit set, absolute column
+    public static absoluteColumnMultiplier : number = 2 * CellEncoder.absoluteRowMultiplier;          // if bit set, absolute column
     
-    public static startPoint : number = 2048;
+    public static startPoint : number = 2048; // Start the encoding of the cell at this Unicode value
 
     public static encode(col: number, row: number, absoluteColumn: boolean = false, absoluteRow: boolean = false) : number {
-	console.log("encode: col = (" + col + ")");
-	console.log("encode: row = (" + row + ")");
 	const addAbsolutes = Number(absoluteRow) * CellEncoder.absoluteRowMultiplier
 	    + Number(absoluteColumn) * CellEncoder.absoluteColumnMultiplier;
 	return addAbsolutes + CellEncoder.maxRows * (CellEncoder.maxColumns / 2 + col)
@@ -26,11 +24,7 @@ class CellEncoder {
     }
     
     public static encodeToChar(col: number, row: number, absoluteColumn: boolean = false, absoluteRow: boolean = false) : string {
-	console.log("col = " + col + ", row = " + row);
-	console.log("encoded num = " + CellEncoder.encode(col, row, absoluteColumn, absoluteRow));
 	const chr = String.fromCodePoint(CellEncoder.encode(col, row, absoluteColumn, absoluteRow));
-	console.log('chr = ' + chr);
-	console.log('code = ' + chr.codePointAt(0));
 	return chr;
     }
 
@@ -45,11 +39,7 @@ class CellEncoder {
     }
 
     public static decodeFromChar(chr: string): [number, number, boolean, boolean] {
-	console.log("decodeFromChar(" + chr + ")");
 	let decodedNum = chr.codePointAt(0);
-	console.log("decodedNum = " + decodedNum);
-	console.log("absolute column multiplier = " + CellEncoder.absoluteColumnMultiplier);
-	console.log("absolute row multiplier = " + CellEncoder.absoluteRowMultiplier);
 	let absoluteColumn = false;
 	let absoluteRow = false;
 	if (decodedNum & CellEncoder.absoluteRowMultiplier) {
@@ -60,9 +50,6 @@ class CellEncoder {
 	    decodedNum &= ~CellEncoder.absoluteColumnMultiplier;
 	    absoluteColumn = true;
 	}
-	console.log("decoded num = " + decodedNum);
-	console.log("absolute column: " + absoluteColumn);
-	console.log("absolute row: " + absoluteRow);
 	const result : [number, number, boolean, boolean] = [CellEncoder.decodeColumn(decodedNum), CellEncoder.decodeRow(decodedNum), absoluteColumn, absoluteRow];
 	return result;
     }
@@ -128,12 +115,8 @@ export class FixDiff {
 	});
     }
     
-    //private static absoluteGreek = "α";
-    //private static relativeGreek = "ρ";
-    
     constructor() {
 	this.initArrays();
-//	console.log(this.fns);
     }
 
     public static toPseudoR1C1(srcCell : string, destCell: string) : string {
@@ -187,49 +170,35 @@ export class FixDiff {
 	for (let i = 0; i < this.fns.length; i++) {
 	    formula = formula.replace(this.fns[i], this.fn2unicode[this.fns[i]]);
 	}
-//	console.log("TOKENIZING " + formula);
 	formula = formula.replace(/(\-?\d+)/g, (_, num) => {
-	    console.log("found " + num);
-	    const replacement = String.fromCodePoint(16384 + parseInt(num));
-	    console.log("replacing with " + replacement);
+	    // Make sure the unicode characters are far away from the encoded cell values.
+	    const replacement = String.fromCodePoint(CellEncoder.absoluteColumnMultiplier * 2 + parseInt(num));
 	    return replacement;
 	});
-//	console.log("formula is now " + formula);
 	return formula;
     }
 
     public detokenize(formula: string) : string {
-	console.log("DETOKENIZING " + formula);
 	for (let i = 0; i < this.fns.length; i++) {
 	    formula = formula.replace(this.fn2unicode[this.fns[i]], this.fns[i]);
 	}
-	console.log("formula is now " + formula);
 	return formula;
     }
     
     // Return the diffs (with formulas treated specially).
     public compute_fix_diff(str1, str2, c1, r1, c2, r2) { //c2, r2) {
-	// First, "tokenize" the strings.
-	console.log(str1);
-	console.log(str2);
-	// Convert to R1C1 format.
+	// Convert to pseudo R1C1 format.
 	let rc_str1 = FixDiff.formulaToPseudoR1C1(str1, c1, r1); // ExcelUtils.formulaToR1C1(str1, c1, r1);
 	let rc_str2 = FixDiff.formulaToPseudoR1C1(str2, c2, r2); // ExcelUtils.formulaToR1C1(str2, c2, r2);
-	console.log("Pseudo R1C1: ");
-	console.log(rc_str1);
-	console.log(rc_str2);
+	// Tokenize the functions.
 	rc_str1 = this.tokenize(rc_str1);
 	rc_str2 = this.tokenize(rc_str2);
-	console.log("Pseudo R1C1 tokenized: ");
-	console.log(rc_str1);
-	console.log(rc_str2);
 	// Build up the diff.
 	let theDiff = diff.main(rc_str1, rc_str2);
-	console.log("diff " + JSON.stringify(theDiff));
 	// Now de-tokenize the diff contents
-	// and convert back out of R1C1 format.
+	// and convert back out of pseudo R1C1 format.
 	for (let j = 0; j < theDiff.length; j++) {
-	    console.log("processing " + JSON.stringify(theDiff[j][1]));
+	    // console.log("processing " + JSON.stringify(theDiff[j][1]));
 	    if (theDiff[j][0] == 0) { // No diff
 		theDiff[j][1] = this.fromPseudoR1C1(theDiff[j][1], c1, r1); ///FIXME // doesn't matter which one
 	    } else if (theDiff[j][0] == -1) { // Left diff
@@ -237,7 +206,6 @@ export class FixDiff {
 	    } else { // Right diff
 		theDiff[j][1] = this.fromPseudoR1C1(theDiff[j][1], c2, r2);
 	    }
-	    console.log("about to detokenize " + JSON.stringify(theDiff[j][1]));
 	    theDiff[j][1] = this.detokenize(theDiff[j][1]);
 	}
 	diff.cleanupSemantic(theDiff);
@@ -284,17 +252,14 @@ export class FixDiff {
 
     public fromPseudoR1C1(r1c1_formula : string, origin_col: number, origin_row: number) : string {
 	// We assume that formulas have already been 'tokenized'.
-	console.log("fromPseudoR1C1 = " + r1c1_formula + ", origin_col = " + origin_col + ", origin_row = " + origin_row);
+	// console.log("fromPseudoR1C1 = " + r1c1_formula + ", origin_col = " + origin_col + ", origin_row = " + origin_row);
 	let r1c1 = r1c1_formula.slice();
 	// Find the Unicode characters and decode them.
 	r1c1 = r1c1.replace(/([\u800-\uF000])/g, (_full, encoded_char) => {
 	    if (encoded_char.codePointAt(0) < CellEncoder.startPoint) {
 		return encoded_char;
 	    }
-	    console.log("encoded char = (" + encoded_char + ")");
-	    console.log("value of char = " + encoded_char.codePointAt(0));
 	    let [co, ro, absCo, absRo] = CellEncoder.decodeFromChar(encoded_char);
-	    console.log(JSON.stringify([ro, co]));
 	    let result: string;
 	    if (!absCo && !absRo) {
 		// Both relative (R[..]C[...])
@@ -312,7 +277,6 @@ export class FixDiff {
 		// Both absolute (R...C...)
 		result = '$'  + ExcelUtils.column_index_to_name(co) + '$' + ro;
 	    }
-	    console.log("converted to " + result);
 	    return result;
 	});
 	return r1c1;
@@ -357,37 +321,9 @@ let [row2, col2] = [1, 3];
 let str1 = '=ROUND($A1:B9)'; // 'ROUND(A1)+12';
 let str2 = '=ROUND($A1:C10)'; // 'ROUNDUP(B2)+12';
 
-console.log(ExcelUtils.column_index_to_name(col1) + row1);
-console.log(ExcelUtils.column_index_to_name(col2) + row2);
-//console.log(str1);
-//console.log(str2);
-
 let diffs = nd.compute_fix_diff(str1, str2, col1 - 1, row1 - 1, col2 - 1, row2 - 1);
 console.log(JSON.stringify(diffs));
 let [a, b] = nd.pretty_diffs(diffs);
-
-if (false) {
-    
-    console.log(".....");
-    
-    
-    console.log(CellEncoder.maxEncodedSize());
-    let x = CellEncoder.encodeToChar(3, 4);
-    console.log(x);
-    console.log(JSON.stringify(CellEncoder.decodeFromChar(x)));
-    
-
-
-
-    let encoded = FixDiff.toPseudoR1C1("C10", "B10");
-    
-    console.log(encoded);
-    console.log(JSON.stringify(CellEncoder.decodeFromChar(encoded)));
-    
-    let encodedFormula = FixDiff.formulaToPseudoR1C1("=ROUND(B1:B10)", 2, 11);
-    console.log("encoded formula = " + encodedFormula);
-
-}
 
 console.log(a);
 console.log("---");
