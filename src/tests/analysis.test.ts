18ce655beaf553f3469a4363b7de38c4a2f9e868
Daniel W. Barowy <dbarowy@cs.williams.edu>
2022-04-17 19:09:22 -0400

    update test for one-based coordinate vectors

--- src/tests/analysis.test.ts
+++ src/tests/analysis.test.ts
@@ -5,16 +5,16 @@ describe("Analysis.relativeFormulaRefs", () => {
   it("should correctly extract range refs", () => {
     const sheetName = "Sheet 1";
     const fs = new XLNT.Dictionary<string>();
-    const fLoc = new XLNT.ExceLintVector(0, 5, 0);
+    const fLoc = new XLNT.ExceLintVector(1, 6, 0);
     fs.put(fLoc.asKey(), "=SUM(A1:A5)");
     const vs = Analysis.relativeFormulaRefs(fs, sheetName);
     const expected = new XLNT.Dictionary<XLNT.ExceLintVector[]>();
     expected.put(fLoc.asKey(), [
-      new XLNT.ExceLintVector(0, -1, 0),
-      new XLNT.ExceLintVector(0, -2, 0),
-      new XLNT.ExceLintVector(0, -3, 0),
-      new XLNT.ExceLintVector(0, -4, 0),
       new XLNT.ExceLintVector(0, -5, 0),
+      new XLNT.ExceLintVector(0, -4, 0),
+      new XLNT.ExceLintVector(0, -3, 0),
+      new XLNT.ExceLintVector(0, -2, 0),
+      new XLNT.ExceLintVector(0, -1, 0),
     ]);
     expect(vs).toEqual(expected);
   });
